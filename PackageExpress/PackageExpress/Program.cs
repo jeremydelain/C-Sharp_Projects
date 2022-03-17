2235b150d1961ca781e6629f7ca57c1ee2d4f63a
jeremydelain <jeremydelain@gmail.com>
2022-03-17 18:08:10 -0500

    Step: 229
    While and Do While loops, a timer and number guessing game

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -9,15 +9,18 @@ namespace PackageExpress
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
 
+            //collect package weight
             Console.WriteLine("Please enter package weight:");
             float weight = float.Parse(Console.ReadLine());
             if (weight > 50)
             {
+                //inform user their package exceeds the limit and terminate the app after 5 seconds
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Thread.Sleep(5000);
                 Environment.Exit(0);
             }
 
+            //collect package dimensions
             Console.WriteLine("Please enter package width:");
             float width = float.Parse(Console.ReadLine());
             Console.WriteLine("Please enter package height:");
@@ -26,11 +29,13 @@ namespace PackageExpress
             float length = float.Parse(Console.ReadLine());
             if (width + height + length > 50)
             {
+                //inform user their package exceeds the limit and terminate the app after 5 seconds
                 Console.WriteLine("Package too big to be shipped via Package Express.");
                 Thread.Sleep(5000);
                 Environment.Exit(0);
             }
 
+            //return a formatted String with the user's quote
             float quote = weight * width * height * length / 100;
             Console.WriteLine("Your estimated total for shipping this package is: " + quote.ToString("C"));
             Console.WriteLine("Thank you!");
---
+++ WhileStatements/WhileStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace WhileStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number of seconds to start a timer:");
+            int seconds = int.Parse(Console.ReadLine());
+            do
+            {
+                Thread.Sleep(1000);
+                seconds--;
+                Console.WriteLine(seconds.ToString());
+            } while (seconds > 0);
+
+            Console.WriteLine("Guess what number I'm thinking of between 1 and 5");
+            Random rnd = new Random();
+            int secretNum = rnd.Next(1, 6);
+            int guess = int.Parse(Console.ReadLine());
+            while (guess != secretNum)
+            {
+                Console.WriteLine("INCORRECT");
+                guess = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("CORRECT :)");
+            Thread.Sleep(5000);
+        }
+    }
+}
