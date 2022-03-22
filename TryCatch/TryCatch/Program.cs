03f1415f7a575dc882faf5621958b8826ff415f6
jeremydelain <jeremydelain@gmail.com>
2022-03-22 13:21:52 -0500

    Step: 263
    Try/catch exercise

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> integers = new List<int>()
+            {
+                10, 50, 100, 250, 500, 1000
+            };
+
+            Console.WriteLine("Enter non-zero int to divide list elements by:");
+            
+            try
+            {
+                int divide = int.Parse(Console.ReadLine());
+                for (int i = 0; i < integers.Count; i++)
+                {
+                    Console.WriteLine(integers[i] / divide);
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("CANNOT DIVIDE BY A STRING");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("CANNOT DIVIDE BY ZERO");
+            }
+            
+
+            Console.WriteLine("Program continues");
+            Console.ReadLine();
+        }
+    }
+}
