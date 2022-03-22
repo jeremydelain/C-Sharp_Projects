b84a763ea3a076b838a05a954956e20b6e2ae121
jeremydelain <jeremydelain@gmail.com>
2022-03-22 17:20:46 -0500

    Step: 281
    method drill

---
+++ Step281/Step281/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    class myClass
+    {
+        public void myMethod(int a, int b)
+        {
+            a += 100;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Step281/Step281/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass mine = new myClass();
+            mine.myMethod(1, 2);
+            mine.myMethod(a: 1, b: 2);
+        }
+    }
+}
