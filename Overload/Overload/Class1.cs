e362becdd461c5a217c7e2f1b27be9c202b5ba14
jeremydelain <jeremydelain@gmail.com>
2022-03-22 15:47:44 -0500

    Step: 279
    Overloaded method drill

---
+++ Overload/Overload/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class MathOperation
+    {
+        public int AddTen(int input)
+        {
+            return input + 10;
+        }
+
+        public int AddTen(double input)
+        {
+            return (int)input + 10;
+        }
+
+        public int AddTen(string input)
+        {
+            return int.Parse(input) + 10;
+        }
+    }
+}
---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiate MathOperation and call 3 overloaded methods
+            MathOperation math = new MathOperation();
+            Console.WriteLine(math.AddTen(5));
+            Console.WriteLine(math.AddTen(5.5));
+            Console.WriteLine(math.AddTen("5"));
+            
+            Console.ReadLine();
+        }
+    }
+}
