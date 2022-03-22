cac0685e42adb6d681596607dd42461ee92e6095
jeremydelain <jeremydelain@gmail.com>
2022-03-22 16:38:53 -0500

    Step: 280
    method drill

---
+++ Step280/Step280/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step280
+{
+    class OptionalParam
+    {
+        //add inputs and return as int
+        public int AddInputs(int a, int b = 0)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Step280/Step280/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalParam opt = new OptionalParam();
+
+            //collect 1-2 inputs from user
+            Console.WriteLine("Enter two integers. The second may be blank. Your entries will be added and returned:");
+            int a = int.Parse(Console.ReadLine());
+            string userInputB = Console.ReadLine();
+
+            //check if user gave two inputs
+            try
+            {
+                int b = int.Parse(userInputB);
+                Console.WriteLine(opt.AddInputs(a, b));
+            }
+            //evaluate with one input
+            catch(FormatException e)
+            {
+                Console.WriteLine(opt.AddInputs(a));
+            }
+            Console.ReadLine();
+        }
+    }
+}
