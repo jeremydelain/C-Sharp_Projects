63c71f6eb32b9b5bbf0397cd3ab3c48d895a4395
jeremydelain <jeremydelain@gmail.com>
2022-03-22 14:24:22 -0500

    Step: 275
    class drill

---
+++ CallingMethods/CallingMethods/MathOps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class MathOps
+    {
+        public int MathAdd(int userInput)
+        {
+            return userInput + 10;
+        }
+        public int MathMultiply(int userInput)
+        {
+            return userInput * 10;
+        }
+        public int MathSub(int userInput)
+        {
+            return userInput - 10;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get int from user
+            Console.WriteLine("Enter an integer to have math operations performed on:");
+            int userInput = int.Parse(Console.ReadLine());
+            MathOps math = new MathOps();
+
+            //add 10
+            Console.WriteLine(userInput + " + 10 = ");
+            Console.WriteLine(math.MathAdd(userInput));
+
+            //multiply by 10
+            Console.WriteLine(userInput + " x 10 = ");
+            Console.WriteLine(math.MathMultiply(userInput));
+
+            //subtract 10
+            Console.WriteLine(userInput + " - 10 = ");
+            Console.WriteLine(math.MathSub(userInput));
+
+            Console.ReadLine();
+        }
+    }
+}
