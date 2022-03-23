7770c0fb5f06a5f1fabc30779641ea3ad46218db
jeremydelain <jeremydelain@gmail.com>
2022-03-23 15:53:15 -0500

    Step: 302
    abstract class example

---
+++ Step302/Step302/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step302
+{
+    class Employee : Person
+    {
+        override public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ Step302/Step302/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee foo = new Employee();
+            foo.firstName = "Sample";
+            foo.lastName = "Student";
+            foo.SayName();
+            Console.ReadLine();
+        }
+    }
+}
