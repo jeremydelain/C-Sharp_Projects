b56c8e1b9b88efba705fe7dc221c11fef16f55a2
jeremydelain <jeremydelain@gmail.com>
2022-03-23 16:06:34 -0500

    Step: 306
    Interface implementation

---
+++ Step306/Step306/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step306
+{
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Press ENTER to QUIT:");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Step306/Step306/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step306
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee foo = new Employee();
+            foo.Quit();
+        }
+    }
+}
