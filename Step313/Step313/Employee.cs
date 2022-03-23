b271d4d111f67f13c55b42ebef124012f0e6a40f
jeremydelain <jeremydelain@gmail.com>
2022-03-23 16:49:58 -0500

    Step: 313
    Overloading operators

---
+++ Step313/Step313/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step313
+{
+    class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+    }
+}
---
+++ Step313/Step313/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee();
+            one.firstName = "John";
+            one.lastName = "Deere";
+            one.Id = 1000;
+
+            Employee two = new Employee();
+            two.firstName = "Steve";
+            two.lastName = "Rodgers";
+            two.Id = 1000;
+
+            Employee three = new Employee();
+            three.firstName = "Sarah";
+            three.lastName = "Haras";
+            three.Id = 1001;
+
+            Console.WriteLine(one == two);
+            Console.WriteLine(one == three);
+            Console.WriteLine(three == two);
+            Console.ReadLine();
+        }
+    }
+}
