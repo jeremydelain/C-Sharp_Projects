0630e92055eccbfd0d2e6950f97c7c93f45fa04b
jeremydelain <jeremydelain@gmail.com>
2022-03-29 12:01:14 -0500

    Step: 383

---
+++ Step383/Step383/Foo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383
+{
+    class Foo
+    {
+        private int id;
+        private string name;
+
+        public Foo() : this(0, "")
+        {
+        }
+
+        public Foo(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Foo(int id) : this(id, "")
+        {
+        }
+
+        public Foo(string name) : this(0, name)
+        {
+        }
+    }
+}
---
+++ Step383/Step383/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 100;
+            var str = "string";
+        }
+    }
+}
