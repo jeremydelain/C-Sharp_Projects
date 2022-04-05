7e918b19558e14918cc0656f34151de139961662
jeremydelain <jeremydelain@gmail.com>
2022-04-05 10:19:46 -0500

    Step: 546

---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Jeremy" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
